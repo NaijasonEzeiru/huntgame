c10e0335002af1aafeb3ca36c02c1678deeb4275
NaijasonEzeiru <jayseehe1035@gmail.com>
2024-10-27 21:23:07 +0100

    ..........................

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
 
 const defaultTheme = require("tailwindcss/defaultTheme");
 
@@ -15,10 +16,26 @@ export default {
         // primaryCta: "var(--primary-cta-color)",
         // disabledColor: 'var(--disabled-gold-color)'
       },
+      textShadow: {
+        sm: "0 1px 2px var(--tw-shadow-color)",
+        DEFAULT: "0 2px 4px var(--tw-shadow-color)",
+        lg: "0 8px 16px var(--tw-shadow-color)",
+      },
       fontFamily: {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "text-shadow": (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme("textShadow") }
+      );
+    }),
+  ],
 };
